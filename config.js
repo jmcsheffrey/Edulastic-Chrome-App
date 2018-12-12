a7fe25eb6a9f56c01c574f948013f1501cae82a5
Joe McSheffrey <jmcsheffrey@sscps.org>
2018-12-12 10:00:14 -0500

    disabled kiosk mode

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ window.config = {
    "enableReloadBttn": false,
    "enableLogoutBttn": false,
    "termsOfService": "This device is property of South Shore Charter Public School (SSCPS).  Unauthorized access is prohibited.  Please notify SSCPS immediately that you've found a this device by calling 781-982-4202.  This device should also be returned to SSCPS at 100 Longwater Circle, Norwell, MA 02061 immediately.  Thank you for your help and cooperation.",
-   "kioskEnabled": true
+   "kioskEnabled": false
 };
