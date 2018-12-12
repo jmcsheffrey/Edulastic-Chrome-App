b550e5cdc33ce54669db62b8c85d1eb6054f54f9
Joe McSheffrey <jmcsheffrey@sscps.org>
2018-12-12 09:28:50 -0500

    copied from missing device app

---
+++ config.js
@@ -0,0 +1,12 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "SSCPS Lockout",
+   "homepage": "https://sites.google.com/sscps.org/missingdevice",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "termsOfService": "This device is property of South Shore Charter Public School (SSCPS).  Unauthorized access is prohibited.  Please notify SSCPS immediately that you've found a this device by calling 781-982-4202.  This device should also be returned to SSCPS at 100 Longwater Circle, Norwell, MA 02061 immediately.  Thank you for your help and cooperation.",
+   "kioskEnabled": true
+};
